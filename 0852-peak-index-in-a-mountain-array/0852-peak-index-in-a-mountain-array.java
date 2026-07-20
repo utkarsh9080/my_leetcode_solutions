2cc8786429389cc52b4453baba3e32811babb977
utkarsh Jha <utkarshofc@gmail.com>
2026-07-20 11:07:09 +0530

    Update 0852-peak-index-in-a-mountain-array.java

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -13,3 +13,24 @@ class Solution {
         return max[1];
     }
 }
+
+
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int start=0;
+//         int end=arr.length-1;
+//         int index=0;
+//         while(start<=end){
+//             int mid=(start+end)/2;
+//             if(mid!=0 && mid!=arr.length-1&& arr[mid]>arr[mid+1] & arr[mid]>arr[mid-1] ){
+//                 return mid;
+//             }else if(arr[mid]<arr[mid+1]){
+//                 start=mid+1;
+//             }else{
+//                 end=mid-1;
+//             }
+           
+//         }
+//          return index;
+//     }
+// }
