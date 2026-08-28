7756382e65f8ddbfa5db78ad671730e7c8084875
utkarsh Jha <utkarshofc@gmail.com>
2026-08-28 10:20:12 +0530

    Time: 0 ms (100%), Space: 45.1 MB (72.09%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,29 +1,54 @@
+// class Solution {
+//     public int missingMultiple(int[] nums, int k) {
+//         HashSet<Integer> h = new HashSet<>();
+//         for(int x:nums){
+//             h.add(x);
+//         }   
+//         int temp =k;
+//         // for(int i=0;i<h.size();i++){
+//         //     if(search(h,k)){
+//         //         k=k+temp;
+//         //     }else{
+//         //         return k;
+//         //     }
+//         // }
+//         // return k+temp;
+//         while(search(h,k)){
+//             k+=temp;
+//         }
+//         return k;
+//     }
+//     static boolean search(HashSet<Integer> h,int k){
+//         for(int num:h){
+//             if(num==k){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        HashSet<Integer> h = new HashSet<>();
-        for(int x:nums){
-            h.add(x);
-        }   
-        int temp =k;
-        // for(int i=0;i<h.size();i++){
-        //     if(search(h,k)){
-        //         k=k+temp;
-        //     }else{
-        //         return k;
-        //     }
-        // }
-        // return k+temp;
-        while(search(h,k)){
-            k+=temp;
-        }
-        return k;
-    }
-    static boolean search(HashSet<Integer> h,int k){
-        for(int num:h){
-            if(num==k){
-                return true;
+         int x = k;
+
+        while (true) {
+            boolean found = false;
+
+            for (int num : nums) {
+                if (num == x) {
+                    found = true;
+                    break;
+                }
             }
+
+            if (!found) {
+                return x;
+            }
+
+            x += k;
         }
-        return false;
     }
 }
