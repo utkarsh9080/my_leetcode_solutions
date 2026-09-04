ac214d7547defd59434fe9ea4e02491db7033575
utkarsh Jha <utkarshofc@gmail.com>
2026-09-04 15:50:28 +0530

    Time: 1 ms (99.79%), Space: 46.7 MB (15.74%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,22 +1,42 @@
+// class Solution {
+//     public int firstStableIndex(int[] nums, int k) {
+//         if(nums.length==1) return 0;
+//         int l=0;
+//         while(l<nums.length){
+//             int max=Integer.MIN_VALUE;
+//             int min = Integer.MAX_VALUE;
+//             for(int i=0;i<=l;i++){
+//                 max = Math.max(max,nums[i]);
+//             }
+//             for(int j=l;j<nums.length;j++){
+//                 min=Math.min(min,nums[j]);
+//             }
+//             if((max-min)<=k){
+//                 return l;
+//             }
+//             l++;
+//         }
+//         return -1;
+        
+//     }
+// }
+
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        if(nums.length==1) return 0;
-        int l=0;
-        while(l<nums.length){
-            int max=Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
-            for(int i=0;i<=l;i++){
-                max = Math.max(max,nums[i]);
+        int n=nums.length;
+        int[] right=new int[n];
+        right[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+         right[i]=Math.min(right[i+1],nums[i]);
+        }
+        int left=0;
+        for(int i=0;i<n;i++){
+            left=Math.max(left,nums[i]);
+            if(left-right[i]<=k){
+                return i;
             }
-            for(int j=l;j<nums.length;j++){
-                min=Math.min(min,nums[j]);
-            }
-            if((max-min)<=k){
-                return l;
-            }
-            l++;
         }
         return -1;
-        
     }
+
 }
