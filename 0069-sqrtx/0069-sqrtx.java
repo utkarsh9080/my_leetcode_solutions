3c1a9b3f5eeb6a2a413d92912dd5fc334545267b
utkarsh Jha <utkarshofc@gmail.com>
2026-09-11 01:42:43 +0530

    Time: 1 ms (99.05%), Space: 42.9 MB (8.71%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        long low = 1;
+        long high=x;
+        long ans=1;
+        while(low<=high){
+            long mid= low+(high-low)/2;
+            if(mid*mid==x){
+                return (int) mid;
+            }
+            if(mid*mid<x){
+                ans=mid;
+                low=mid+1;
+            }
+            if(mid*mid>x){
+                high=mid-1;
+            }
+        }
+        return (int) ans;
+    }
+}
