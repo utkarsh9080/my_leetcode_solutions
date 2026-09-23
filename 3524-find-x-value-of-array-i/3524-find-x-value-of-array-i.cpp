563f237e60d3e02a1453053f54f1a0009fd09616
utkarsh Jha <utkarshofc@gmail.com>
2026-09-23 17:34:08 +0530

    Time: 162 ms (35.78%), Space: 151 MB (70.27%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> result(k, 0);
+        vector<long long> prevcount(k, 0);
+        for (int i = 0; i < n; i++) {
+            // index i par end hone waale saare subarrays
+            vector<long long> currcount(k, 0);
+
+            int currElementRemainder = nums[i] % k;
+            currcount[currElementRemainder]++;
+
+            for (int oldRem = 0; oldRem <= k - 1; oldRem++) {
+                // naya rem nikalna by taking the previous remainders value
+                int newRem = ((long long)oldRem * nums[i] % k) % k;
+
+                currcount[newRem] += prevcount[oldRem];
+            }
+
+            prevcount = move(currcount);
+            // std::move to not copy the thing in stack continuesly
+            for (int x = 0; x <= k - 1; x++) {
+                result[x] += prevcount[x];
+            }
+        }
+        return result;
+    }
+};
